b99cf0aa6aaab60ed44e16b3171d72a64aff7d38
Bart Massey <bart.massey@gmail.com>
2020-12-13 02:49:21 -0800

    miller-rabin primality tester in Rust

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+//! Miller-Rabin probabilistic test for composite numbers
+//! as described in Corman/Leiserson/Rivest.
+//!
+//! Translated from a 1999 [Nickle](http://nickle.org) example.
+
+use num_bigint::*;
+use rand::prelude::*;
+
+struct Witness {
+    pow: BigUint,
+    wit: BigUint,
+}
+
+/// Modified version of bigpowmod() from numbers.5c.
+/// Computes core of Miller-Rabin test as suggested by
+/// Cormen/Leiserson/Rivest.
+fn witnessexp(b: &BigUint, e: &BigUint, m: &BigUint) -> Witness {
+    if *e == BigUint::from(0u8) {
+        return Witness { pow: BigUint::from(0u8), wit: BigUint::from(1u8) };
+    }
+    if *e == BigUint::from(1u8) {
+        return Witness { pow: b % m, wit: BigUint::from(0u8)};
+    }
+    let ehalf = e / BigUint::from(2u8);
+    let mut tmp = witnessexp(b, &ehalf, m);
+    if tmp.wit != BigUint::from(0u8) {
+        return tmp;
+    }
+    let t: BigUint = BigUint::pow(&tmp.pow, 2) % m;
+    if t == BigUint::from(1u8) && &tmp.pow != &BigUint::from(1u8) && &tmp.pow + BigUint::from(1u8) != *m  {
+        tmp.wit = tmp.pow;
+        tmp.pow = t;
+        return tmp;
+    }
+    if e % BigUint::from(2u8) == BigUint::from(0u8) {
+        tmp.pow = t;
+    } else {
+        tmp.pow = (t * b) % m;
+    }
+    tmp
+}
+
+/// Rest of Miller-Rabin test.
+fn witness(a: &BigUint, n: &BigUint) -> bool {
+    let ndown = n - &BigUint::from(1u8);
+    let we = witnessexp(a, &ndown, n);
+    we.wit != BigUint::from(0u8) || we.pow != BigUint::from(1u8)
+}
+
+/// Returns false for all prime n, and true for most
+/// composite n. Failure probability is 1/2**d.
+fn composite(n: &BigUint, d: u64) -> bool {
+    let mut rng = rand::thread_rng();
+    let primes = [2u8, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    for p in &primes {
+        let p = BigUint::from(*p);
+        if *n == p {
+            return false;
+        }
+        if n % p == BigUint::from(0u8) {
+            return true;
+        }
+    }
+    for _ in 0 .. d {
+        let a = rng.gen_range(BigUint::from(1u8), n);
+        if witness(&a, &n) {
+            return true;
+        }
+    }
+    false
+}
+
+fn main() {
+    let argv: Vec<String> = std::env::args().collect();
+    let n = argv[1].parse().unwrap();
+    let d = if argv.len() > 2 {
+        argv[2].parse().unwrap()
+    } else {
+        1024
+    };
+    println!("{}", !composite(&n, d));
+}
